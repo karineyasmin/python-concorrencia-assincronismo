fd0baa210b4f4d569012757883338ea7fc06bb00
Karine Yasmin <karine.yasmin@outlook.com>
2024-11-30 19:56:14 -0300

    updated

--- sessao_multiprocessamento/processos/pool_python_v1.py
+++ sessao_multiprocessamento/processos/pool_python_v1.py
@@ -5,16 +5,10 @@ def calcular(dado):
     return dado**2
 
 
-def imprimir_nome_processo():
-    print(f"Iniciando o processo com o nome: {multiprocessing.current_process().name}")
-
-
 def main():
     tamanho_pool = multiprocessing.cpu_count() * 2
     print(f"tamanho da Pool: {tamanho_pool}")
-    pool = multiprocessing.Pool(
-        processes=tamanho_pool, initializer=imprimir_nome_processo
-    )
+    pool = multiprocessing.Pool(processes=tamanho_pool)
     entradas = list(range(7))
     saidas = pool.map(calcular, entradas)
     print(f"Saídas: {saidas}")
